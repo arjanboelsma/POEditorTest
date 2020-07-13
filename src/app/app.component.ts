590d8dd0a077a494e5c65a94cbf555332e88b121
Arjan Boelsma <arjan.boelsma@dynalinq.eu>
2020-07-13 08:49:51 +0200

    Working with language switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'translation-test';
   user = 'Arjan';
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+  }
+
+  setLang(lang): void {
+    this.translate.setDefaultLang(lang);
+  }
 }
